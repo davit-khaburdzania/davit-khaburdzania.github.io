44e283615d98a871e955aeafdfe6f31313b2098f
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2014-01-27 17:13:05 +0400

    added compiling and watching less tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,35 @@ module.exports = function (grunt) {
           keepalive: true
         }
       }
+    },
+    
+    less: {
+      options: {
+        path: 'assets'
+      },
+      src: {
+        expand: true,
+        cwd:    'assets/less/',
+        src:    ['*.less'],
+        dest:   'assets/css/', 
+        ext:    '.css'
+      }
+    },
+
+    watch: {
+      files: 'assets/less/*.less',
+      tasks: ['less']
     }
   });
 
   grunt.loadNpmTasks('grunt-nodestatic');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('preview', 'nodestatic:server');
+
+  grunt.registerTask('build', 'build blog', function () {
+    require(__dirname + '/src/app.js');
+
+  });
 };
